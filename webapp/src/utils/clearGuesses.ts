9fd5bcd1ed6a7e7b743cd9fc8c315150a8a6fcc5
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2021-10-24 21:03:50 +0200

    reset game including hints

--- webapp/src/utils/clearGuesses.ts
+++ webapp/src/utils/clearGuesses.ts
@@ -1,4 +1,4 @@
-import { CellType, IGameData, INumberCell } from 'models/cellModels';
+import { CellType, IGameData, IHintCell, INumberCell } from 'models/cellModels';
 
 export const clearGuesses = (game: IGameData): IGameData => {
   const newGame: IGameData = JSON.parse(JSON.stringify(game));
@@ -8,6 +8,19 @@ export const clearGuesses = (game: IGameData): IGameData => {
     .forEach(c => {
       (c as INumberCell).guess = 0;
       (c as INumberCell).pencilMarks = [];
+    });
+
+  newGame.cells
+    .filter(c => c.type === CellType.HintCell)
+    .forEach(c => {
+      const hints = (c as IHintCell).hints;
+      hints.forEach(hint => {
+        if (hint) {
+          hint.sumGuessed = 0;
+          hint.usedDigits = [];
+          hint.combinations.forEach(c => c.excluded = false)
+        }
+      });
     });
 
   return newGame;
